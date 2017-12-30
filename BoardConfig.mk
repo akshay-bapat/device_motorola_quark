1a026788b7a9d421d108469dcb52fb7bb34d0aeb
Artem Borisov <dedsa2002@gmail.com>
2017-12-30 16:03:20 +0300

    Quark: Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,3 +188,13 @@ HWUI_COMPILE_FOR_PERF := true
 
 #Temp workaround
 ALLOW_MISSING_DEPENDENCIES=true
+
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
