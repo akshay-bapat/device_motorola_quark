e9ce4f72cbc466a1151f5c10153825643f7ab94d
Felipe Leon <fglfgl27@gmail.com>
2018-01-17 19:36:34 -0200

    Quark: cm->lineage.mk
    Change-Id: Ica21a7dcb3c144778e0716deb7bdcda55f8d592f
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -1,13 +1,13 @@
 $(call inherit-product, device/motorola/quark/full_quark.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := Moto MAXX
-PRODUCT_NAME := cm_quark
+PRODUCT_NAME := lineage_quark
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
