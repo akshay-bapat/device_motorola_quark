3753481c0a4e8a9e1cdcd8aa59ff772b3ba611f9
Felipe Leon <fglfgl27@gmail.com>
2018-01-19 19:25:03 -0200

    Quark: always dex-preopt
    the size diference is very little
    
    Change-Id: I59528141c81e5e20586e71edc05ee27f08d57e45
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,12 +188,7 @@ HWUI_COMPILE_FOR_PERF := true
 
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
+    WITH_DEXPREOPT := true
 endif
 
 # Use mke2fs to create ext4 images
