849c118ff2d51e4d3f2f8f7191e7d6cc8b46cc65
Felipe Leon <fglfgl27@gmail.com>
2018-01-19 02:17:50 -0200

    Quark: Update liblight
    Change-Id: Idecb809eb2f444c9152be1592ef6f7fa72102295
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- liblight/lights.c
+++ liblight/lights.c
@@ -162,36 +162,31 @@ static int
 set_speaker_light_locked(struct light_device_t* dev,
         struct light_state_t const* state)
 {
+    unsigned int brightness = 0;
     unsigned long onMS, offMS;
-    char onMSC[PAGE_SIZE];
-    char offMSC[PAGE_SIZE];
-    char value[PROPERTY_VALUE_MAX];
-    unsigned int brightness;
-    int statenow;
 
     switch (state->flashMode) {
         case LIGHT_FLASH_TIMED:
             onMS = state->flashOnMS;
             offMS = state->flashOffMS;
             break;
-        case LIGHT_FLASH_NONE:
         default:
             onMS = 0;
             offMS = 0;
             break;
     }
 
-    statenow = rgb_to_brightness(state);
-    // If a brightness has been applied by the user
-    brightness = (g_notification.color & 0xFF000000) >> 24;
-    if (brightness == 0x00) {
-        brightness = 0xFF;
-    }
-    if (statenow > 0) { 
+    if (onMS > 0) {
+        brightness = (g_notification.color & 0xFF000000) >> 24;
+        if (brightness == 0x00) {
+            brightness = 0xFF;
+        }
+        char onMSC[PAGE_SIZE];
+        char offMSC[PAGE_SIZE];
         sprintf(onMSC, "%lu", onMS);
         sprintf(offMSC, "%lu", offMS);
-        if (brightness == 255) brightness = 15; // max_brightness 15 steps of 5
-        else if (brightness > 1 && brightness < 255) brightness = 10;
+        if (brightness > 170) brightness = 15; // max_brightness 15 steps of 5
+        else if (brightness > 85) brightness = 10;
         else brightness = 5;
 
         if ((offMS - 0) > 0) {
@@ -204,18 +199,18 @@ set_speaker_light_locked(struct light_device_t* dev,
             write_str(LED_TRIGER, "none");
             write_int(LED_BRIGHTNESS, brightness);
        }
-
-       ALOGE("liblights notification led brightness=%d, timer 0n=%s, off=%s\n", brightness, onMSC, offMSC);
     } else {
+        char value[PROPERTY_VALUE_MAX];
         // prop used to keep battery triger at battery-full case is enable, I activated this from a app at boot if app switch is on
         property_get("led.batton", value, NULL);
         if (strstr(value, "1")) {
-            write_int(LED_BLINK_BRIGHTNESS, statenow);
-            write_int(LED_BRIGHTNESS, statenow);// prevent led be enable all the time
+            write_int(LED_BLINK_BRIGHTNESS, 0);
+            write_int(LED_BRIGHTNESS, 0);// prevent led be enable all the time
             write_str(LED_TRIGER, "battery-full");
         } else write_str(LED_TRIGER, "none");// none alredy set LED_BRIGHTNESS to 0
     }
 
+    ALOGI("liblights notification led brightness=%d, timer 0n=%ld, off=%ld\n", brightness, onMS, offMS);
     return 0;
 }
 
