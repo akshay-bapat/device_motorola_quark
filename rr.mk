fba66e5a1d9f79528f2e8ee968869430fac0b680
Felipe Leon <fglfgl27@gmail.com>
2018-02-13 07:20:02 -0200

    Quark: update rr.mk
    Change-Id: Ic56365e333b5f17d5f8ee4041f8914057f81c4e6
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- rr.mk
+++ rr.mk
@@ -17,4 +17,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # export the below before . build/envsetup.sh
 # export ROM_VVV=$(grep PRODUCT_VERSION vendor/rr/config/common.mk | head -1 | awk '{print $3}');
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_DISPLAY_ID=RR-O-v$(ROM_VVV)-$(shell date -u +%Y%m%d)-$(RR_BUILD)-$(RR_BUILDTYPE)
+    BUILD_DISPLAY_ID=RR-O-v$(ROM_VVV)-$(shell date -u +%Y%m%d)-quark-$(RR_BUILDTYPE)
